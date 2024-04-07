657c16018dbe08f182acdf8e8540d310a15ae65e
jkan172 <jkan172@auckalnduni.ac.nz>
2024-04-07 18:18:36 +1200

    Added another sub class for Bookings called Booking

---
+++ src/main/java/nz/ac/auckland/se281/Booking.java
@@ -0,0 +1,26 @@
+package nz.ac.auckland.se281;
+
+public class Booking {
+
+  private String venueCode;
+  private String date;
+  private String customerEmail;
+  private String numOfAttendees;
+
+  public Booking(String venueCode, String date, String customerEmail , String numOfAttendees)
+  
+  {
+    this.venueCode = venueCode;
+    this.date = date;
+    this.customerEmail = customerEmail;
+    this.numOfAttendees = numOfAttendees;
+  }
+
+  public String getBookingVenueCode() {
+    return venueCode;
+  }
+
+  public String getDate() {
+    return date;
+  }
+}
